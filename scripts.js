c4b03b6f38e19fb42e6446140950214816484722
Yuni Wu <blog880203@gmail.com>
2025-01-13 14:25:41 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.getElementById('menuItems');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
